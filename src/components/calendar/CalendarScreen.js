83c44f4a87367a81c57b2c5e6488eb9c6654da96
jssmm <jessenalexmercado@gmail.com>
2020-12-28 19:48:50 -0600

    Personalizacion del icono establecida

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale( 'es' );
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add( 2, 'hours' ).toDate(),
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel'
+}]
+
+export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        
+        const style = {
+            backgroundcolor: '#6d0cc3',
+            borderRadius: '0px',
+            opacity: '0.8',
+            display: 'block',
+            color: 'black'
+        }
+
+        return {
+            style
+        }
+    };
+
+
+    return (
+        <div className="calendar-screen">
+            <Navbar />
+            
+
+            <Calendar
+                localizer={localizer}
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                messages= { messages }
+                eventPropGetter= { eventStyleGetter }
+            />
+        </div>
+    )
+}
